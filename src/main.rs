09732670c10cc7c0b368790d14dbed9265d953be
Alice Lee <alice.r.lee@student.tuwien.ac.at>
2023-12-06 20:34:01 +0100

    added file reading, still can only display one line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::time::Duration;
 use std::str::FromStr;
-
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::fs::File;
+use std::result::Result::Ok;
 use crossterm::event::{self, Event};
-use anyhow::*;
 
 pub mod model;
 use model::{Model, RunningState};
@@ -20,8 +22,13 @@ fn main() -> anyhow::Result<()> {
     tui::install_panic_hook();
     let mut terminal = tui::init_terminal()?;
     let mut model = Model::default();
-    let line = "x this is done";
-    model.to_do.push(todo_txt::Task::from_str(line).unwrap());
+    if let Ok(lines) = read_lines("../../todo.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                model.to_do.push(todo_txt::Task::from_str(&ip).unwrap());
+            }
+        }
+    }
 
     while model.running_state != RunningState::Done {
         // Render the current view
@@ -38,6 +45,12 @@ fn main() -> anyhow::Result<()> {
 
     tui::restore_terminal()?;
     Ok(())
+}
+
+fn read_lines<P>(filename: P) -> anyhow::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
 
 /// Convert Event to Message
