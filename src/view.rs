e970333327e88da9fd071928fe5fbe438b99d0c6
Alice Lee <alice.r.lee@student.tuwien.ac.at>
2023-12-06 22:21:59 +0100

    not ugly!

--- src/view.rs
+++ src/view.rs
@@ -3,12 +3,15 @@ use crate::model::*;
 use todo_txt::task::Task;
 use std::vec::Vec;
 
-fn tasks_to_tui_list(task_vec : &mut Vec<Task>, list_vec : &mut Vec<ListItem>) {
+fn tasks_to_list(task_vec : &mut Vec<Task>) -> List {
+    let mut temp = Vec::<ListItem>::new();
+
     for task in task_vec.iter() {
-        list_vec.push(ListItem::new(Line::from(Span::raw(
+        temp.push(ListItem::new(Line::from(Span::raw(
                         format!("{}", task.subject)
                         ))));
     }
+    return List::new(temp)
 }
 
 pub fn view(model:&mut Model, f: &mut Frame) {
@@ -21,30 +24,20 @@ pub fn view(model:&mut Model, f: &mut Frame) {
                      Constraint::Percentage(33),
         ])
         .split(f.size());
-
-    let mut list1 = Vec::<ListItem>::new();
-    let mut list2 = Vec::<ListItem>::new();
-    let mut list3 = Vec::<ListItem>::new();
-    tasks_to_tui_list(&mut model.to_do, &mut list1);
-    tasks_to_tui_list(&mut model.stuck, &mut list2);
-    tasks_to_tui_list(&mut model.today, &mut list3);
-    let todo_list = List::new(list1);
-    let stuck_list = List::new(list2);
-    let today_list = List::new(list3);
     
-    f.render_widget(todo_list
+    f.render_widget(tasks_to_list(&mut model.to_do)
         .block(Block::default()
                .title("To Do")
                .title_alignment(Alignment::Center)
                .borders(Borders::ALL)),
         layout[0]);
-    f.render_widget(today_list
+    f.render_widget(tasks_to_list(&mut model.today)
         .block(Block::default()
                .title("Today")
                .title_alignment(Alignment::Center)
                .borders(Borders::ALL)),
         layout[1]);
-    f.render_widget(stuck_list
+    f.render_widget(tasks_to_list(&mut model.stuck)
         .block(Block::default()
                .title("Stuck")
                .title_alignment(Alignment::Center)
